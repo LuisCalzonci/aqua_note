7242a6cd8959f1925d1e3b8b4edced6f5a193cf5
LuisCalzonci <71913975+LuisCalzonci@users.noreply.github.com>
2021-01-07 09:50:22 -0600

    mi primer commit

---
+++ README.md
@@ -0,0 +1,4 @@
+aqua_note
+=========
+
+A Symfony project created on January 6, 2021, 2:50 pm.
---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,31 @@
+<?php 
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class GenusController extends Controller
+{
+/**
+ * @Route("/genus/{genusName}")
+ */
+	public function showAction($genusName)
+	{
+		$notes =[
+			'holaa, los calamares dicen hola',
+			'se dice que son buena honda xd',
+			'inked'
+		];
+
+        return $this->render('genus/show.html.twig', array(
+            'name' => $genusName,
+            'notes' => $notes 
+        ));
+
+		// return new Response($html);
+	}
+}
+
+ ?>

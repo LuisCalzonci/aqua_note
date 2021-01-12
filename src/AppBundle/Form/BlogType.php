7d52efa1dba22e03e84935d9f0d61a0edc4fe5cd
laguillon <luis.aguillon@harman.com>
2021-01-12 13:54:47 -0600

    addTemplates whit boostrap

--- src/AppBundle/Form/BlogType.php
+++ src/AppBundle/Form/BlogType.php
@@ -6,6 +6,10 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class BlogType extends AbstractType
 {
